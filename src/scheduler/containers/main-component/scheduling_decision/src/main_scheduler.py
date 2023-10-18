ef7badab18c6420550ebad7a7b9907e6c9f254e0
PaulPuettbach <47384967+PaulPuettbach@users.noreply.github.com>
2023-10-18 17:26:06 +0200

    update for portable

--- src/scheduler/containers/main-component/scheduling_decision/src/main_scheduler.py
+++ src/scheduler/containers/main-component/scheduling_decision/src/main_scheduler.py
@@ -2,6 +2,7 @@ from kubernetes import client, config, watch
 import time
 import json
 import requests
+import concurrent.futures
 from flask import Flask, request
 
 config.load_incluster_config()
@@ -18,44 +19,56 @@ v1=client.CoreV1Api()
 scheduler_name = "EA"
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def result():
-    print(request.form['foo']) # should display 'bar'
-    return 'Received !' # response to your request
+# @app.route('/', methods=['POST'])
+# def result():
+#     print(request.form['foo']) # should display 'bar'
+#     return 'Received !' # response to your request
 
-def nodes_available():
-    ready_nodes = []
-    for n in v1.list_node().items:
-            for status in n.status.conditions:
-                if status.status == "True" and status.type == "Ready":
-                    ready_nodes.append(n.metadata.name)
-    return ready_nodes
+# def nodes_available():
+#     ready_nodes = []
+#     for n in v1.list_node().items:
+#             for status in n.status.conditions:
+#                 if status.status == "True" and status.type == "Ready":
+#                     ready_nodes.append(n.metadata.name)
+#     return ready_nodes
 
-def scheduler(name, node, namespace="default"):
-    body=client.V1Binding()
+# def scheduler(name, node, namespace="default"):
+#     body=client.V1Binding()
         
-    target=client.V1ObjectReference()
-    target.kind="Node"
-    target.apiVersion="v1"
-    target.name= node
+#     target=client.V1ObjectReference()
+#     target.kind="Node"
+#     target.apiVersion="v1"
+#     target.name= node
     
-    meta=client.V1ObjectMeta()
-    meta.name=name
+#     meta=client.V1ObjectMeta()
+#     meta.name=name
     
-    body.target=target
-    body.metadata=meta
+#     body.target=target
+#     body.metadata=meta
     
-    return v1.create_namespaced_binding(name, namespace, body)
+#     return v1.create_namespaced_binding(name, namespace, body)
 
-def main():
+def watch_pod():
     w = watch.Watch()
+    # still in the default namespace
     for event in w.stream(v1.list_namespaced_pod, "default"):
         if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
-            try:
-                #sent the list of the pods to the individual workers
-                res = scheduler(event['object'].metadata.name, nodes_available()[0])
-            except client.rest.ApiException as e:
-                print(json.loads(e.body)['message'])
+            # do something
+
+# only need this if the number of nodes changes
+# def watch_node():
+#     w = watch.Watch()
+#     # still in the default namespace
+#     for event in w.stream(v1.list_namespaced_pod, "default"):
+#         if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
+#             # do something
+
+def main():
+    #only need this if the number fo 
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     n = executor.submit(watch_node)
+    #     p = executor.submit(watch_pod)
+    watch_pod()
                     
 if __name__ == '__main__':
     main()
