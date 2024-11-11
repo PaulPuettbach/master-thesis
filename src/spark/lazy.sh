7485102dc6b482f92a3f30154c4c3d382c51a75b
PaulPuettbach <47384967+PaulPuettbach@users.noreply.github.com>
2024-11-11 22:21:17 +0100

    sync to other station

--- src/spark/lazy.sh
+++ src/spark/lazy.sh
@@ -17,25 +17,11 @@ kubectl apply -f service.account.yaml
 
 echo "-----------------------------------------------------------"
 
-# ./spark-3.5.0-bin-hadoop3/bin/spark-submit \
-#     --master k8s://localhost:6443 \
-#     --deploy-mode cluster \
-#     --class ShortestPath \
-#     --name shortest-path \
-#     --conf spark.kubernetes.namespace=spark-namespace \
-#     --conf spark.executor.instances=5 \
-#     --conf spark.kubernetes.scheduler.name=custom-scheduler \
-#     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
-#     --conf spark.kubernetes.container.image=docker.io/paulpuettbach/spark_image/spark:test \
-    
-    
-#     #local:///opt/spark/benchmark/my-spark-project-1.0.jar
-
 ./spark-3.5.0-bin-hadoop3/bin/spark-submit \
     --master k8s://localhost:6443 \
     --deploy-mode cluster \
-    --class ShortestPath \
-    --name shortest-path \
+    --class science.atlarge.graphalytics.graphx.bfs.BreadthFirstSearchJob \
+    --name bfs \
     --conf spark.kubernetes.namespace=spark-namespace \
     --conf spark.executor.instances=3 \
     --conf spark.kubernetes.scheduler.name=custom-scheduler \
@@ -46,7 +32,30 @@ echo "-----------------------------------------------------------"
     --conf spark.hadoop.fs.s3a.secret.key=minio123 \
     --conf spark.hadoop.fs.s3a.path.style.access=true \
     --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
-    s3a://mybucket/mySparkProject1.0.jar
+    s3a://mybucket/graphalytics-platforms-graphx-0.2-SNAPSHOT.jar \
+    s3a://mybucket/path/to/test-bfs-directed-BFS \
+    s3a://mybucket/test-bfs-directed.e \
+    s3a://mybucket/test-bfs-directed.v\
+    true \
+    s3a://mybucket/output.txt \
+    --parameters '{"sourceVertex": 1}'
+
+# ./spark-3.5.0-bin-hadoop3/bin/spark-submit \
+#     --master k8s://localhost:6443 \
+#     --deploy-mode cluster \
+#     --class ShortestPath \
+#     --name shortest-path \
+#     --conf spark.kubernetes.namespace=spark-namespace \
+#     --conf spark.executor.instances=3 \
+#     --conf spark.kubernetes.scheduler.name=custom-scheduler \
+#     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+#     --conf spark.kubernetes.container.image=paulpuettbach/spark_image/spark:test \
+#     --conf spark.hadoop.fs.s3a.endpoint=http://myminio-hl.minio-tenant.svc.cluster.local:9000 \
+#     --conf spark.hadoop.fs.s3a.access.key=minio \
+#     --conf spark.hadoop.fs.s3a.secret.key=minio123 \
+#     --conf spark.hadoop.fs.s3a.path.style.access=true \
+#     --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
+#     s3a://mybucket/mySparkProject1.0.jar
 
 echo "-----------------------------------------------------------"
 kubectl logs $(kubectl get pods --no-headers -o custom-columns=":metadata.name" -n kube-system | grep '^scheduler' | grep -v 'scheduler-daemon') -n kube-system #-c  init-daemon-service
